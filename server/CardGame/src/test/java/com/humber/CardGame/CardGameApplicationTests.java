19c92c475b7f0fc0ad7757bf11980cad1280fa3c
chanonpu <darknezz.pun@gmail.com>
2025-03-27 13:53:38 -0400

    first commit

---
+++ server/CardGame/src/test/java/com/humber/CardGame/CardGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.CardGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
